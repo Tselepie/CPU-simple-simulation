2e8bee71673a4d078ef2b4870a8af40175cf343b
Tselepie <36919937+Tselepie@users.noreply.github.com>
2022-07-17 14:26:04 +0300

    Update RoundRobin.java

--- RoundRobin.java
+++ RoundRobin.java
@@ -6,7 +6,6 @@ public class RoundRobin extends Scheduler {
 
     public RoundRobin() {
         this.quantum = 1; // default quantum
-        /* TODO: you _may_ need to add some code here */
     }
 
     public RoundRobin(int quantum) {
@@ -15,17 +14,13 @@ public class RoundRobin extends Scheduler {
     }
  //συνάρτηση η οποία προσθέτει τη διεργασία p στη λίστα με τα processes
     public void addProcess(Process p) {
-        /* TODO: you need to add some code here */
         this.processes.add(p);
     }
 //Συνάρτηση η οποία παίρνει παίρνει την επόμενη διεργασία και υλοποιεί τον round robin.
 // Αφού ξεκινήσει η διεργασία να τρέχει ενημερώνεται το burst time και στη θέση του αρχικού μπαίνει αυτό που απομένει αφού μειώθηκε από το quantum
 //Σε περίπτωση που απομένει burst time η διεργασία ξαναμπαίνει ready κ μπαίνει στη λίστα των processes
 //Αν δεν έχει μείνει burst time η διεργασία γίνεται terminated και στο τέλος επιστρέφεται το αποτέλεσμα της
-    public Process getNextProcess() {
-        /* TODO: you need to add some code here
-         * and change the return value */
-        
+    public Process getNextProcess() {        
         Process k=processes.get(0);
         k.run();
         CPU.clock = CPU.clock + quantum - 1;
