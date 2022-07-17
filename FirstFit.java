cda65db40241d6dc661599ab143bcf3ffb513570
Tselepie <36919937+Tselepie@users.noreply.github.com>
2022-07-17 14:24:17 +0300

    Update FirstFit.java

--- FirstFit.java
+++ FirstFit.java
@@ -21,11 +21,6 @@ public class FirstFit extends MemoryAllocationAlgorithm {
         int lastEnd = -1;
 
 
-        /* TODO: you need to add some code here
-         * Hint: this should return the memory address where the process was
-         * loaded into if the process fits. In case the process doesn't fit, it
-         * should return -1. */
-
         for (int i = 0; i < availableBlockSizes.length; i++) {
 
             if (availableBlockSizes[i] >= p.getMemoryRequirements()) {
