c36c939c2e00515ffb92ae1b885ff33798647252
Tselepie <36919937+Tselepie@users.noreply.github.com>
2022-07-17 14:23:09 +0300

    Update BestFit.java

--- BestFit.java
+++ BestFit.java
@@ -9,10 +9,6 @@ public class BestFit extends MemoryAllocationAlgorithm {
     public int fitProcess(Process p, ArrayList<MemorySlot> currentlyUsedMemorySlots) {
         boolean fit = false;
         int address = -1;
-        /* TODO: you need to add some code here
-         * Hint: this should return the memory address where the process was
-         * loaded into if the process fits. In case the process doesn't fit, it
-         * should return -1. */
 
         int minSpace = 99999;
         // Find the proper block for the process
