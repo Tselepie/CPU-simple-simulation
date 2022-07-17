602b43d4270ae204270d8a38a6e7c0c8d65230cd
Tselepie <36919937+Tselepie@users.noreply.github.com>
2022-07-17 14:23:34 +0300

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -16,9 +16,6 @@ public class CPU {
     }
 
     public void run() {
-        /* TODO: you need to add some code here
-         * Hint: you need to run tick() in a loop, until there is nothing else to do... */
-
         int min = Integer.MAX_VALUE;
         int pivot = 0;
 
@@ -66,9 +63,6 @@ public class CPU {
     }
 
     public void tick() {
-        /* TODO: you need to add some code here
-         * Hint: this method should run once for every CPU cycle */
-
         clock++;
     }
 }
